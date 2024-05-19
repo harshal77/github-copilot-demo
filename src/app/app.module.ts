cdef8e12a0424bfc46210c05534c4ec80d5b09f0
Harshal shirke <harshalshirke777@gmail.com>
2024-05-19 20:31:09 +0530

    Harshal :- Setup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/assets/prompts.ts
@@ -0,0 +1,12 @@
+export class Prompts {
+    public static readonly prompts = `
+    create a reactive form which accept the user first name , lastname age and phonenumber using mat input 
+    Create a reactive form  which will accept a user first name last name age and mobile number also first name and last name is mandatory.
+    Define a method called submitForm that, upon determining that the form is valid, creates a new object with a dynamic ID and closes the popup.
+    Declare in the constructor and add imports to make this component appropriate as a mat dailog.
+    Include a close popup feature and direct the new user to it.
+    add editUser and deleteUser methods. Edit user method open the same popup with prefilled value and delete should delete the reocrd from table
+    Add data as a input to the mat dailog in constructor and if data is present the assign user form values
+    `
+
+}
